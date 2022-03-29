682254c3f27cf6bc8046723e68e587ca439bb8e0
takeyu1013 <yuto1013@gmail.com>
2022-03-29 23:47:49 +0900

    🐛

--- main.tf
+++ main.tf
@@ -5,11 +5,12 @@ data "aws_availability_zones" "available" {}
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name            = "vpc"
-  cidr            = "10.0.0.0/16"
-  azs             = data.aws_availability_zones.available.names
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  name               = "vpc"
+  cidr               = "10.0.0.0/16"
+  azs                = data.aws_availability_zones.available.names
+  private_subnets    = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  enable_nat_gateway = true
 }
 
 locals {
